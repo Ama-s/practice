b214d9424d0c2cbc6c108582c382f936f6f252ca
Deborah Arku <arkudeborah123@gmail.com>
2024-07-22 11:22:54 +0100

    toy store manager commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<HashMap> toysInStore = ArrayList<>();
+
+        String [] toys = new String [9];
+        toys[0] = "Buzz Lightyear";
+        toys[1] = "Sheriff Woody";
+        toys[2] = "Mr. Potato Head";
+        toys[3] = "Barbie";
+        toys[4] = "Troll doll";
+        toys[5] = "Tonka Truck";
+        toys[6] = "Elmo";
+        toys[7] = "Big bird";
+        toys[8] = "Cookie Monster";
+
+
+        //Make a list of lists or hashmap to classify the toys.
+        //Then sort the list of lists or hashmap in an ascending order
+        //Print each value
+
+
+
+
+    }
+}
